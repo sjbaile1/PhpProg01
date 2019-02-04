b0ea4aa83101e1466aa0a4dcfe975f34dcb191ac
sjbaile1 <42810015+sjbaile1@users.noreply.github.com>
2019-02-04 13:12:41 -0500

    Add files via upload
    Adding Prog1 files

---
+++ Prog01/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"
+            integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+            crossorigin="anonymous"></script>
+    <link   href="css/bootstrap.min.css" rel="stylesheet">
+    <script src="js/bootstrap.min.js"></script>
+</head>
+
+<body>
+    <div class="container">
+    		<div class="row">
+    			<h3>PHP CRUD Grid</h3>
+    		</div>
+			<div class="row">
+				<p>
+					<a href="create.php" class="btn btn-success">Create</a>
+				</p>
+				
+				<table class="table table-striped table-bordered">
+		              <thead>
+		                <tr>
+		                  <th>Name</th>
+		                  <th>Email Address</th>
+		                  <th>Mobile Number</th>
+		                  <th>Action</th>
+		                </tr>
+		              </thead>
+		              <tbody>
+		              <?php 
+					   require_once 'database.php';
+					   $pdo = Database::connect();
+					   $sql = 'SELECT * FROM customers ORDER BY id DESC';
+	 				   foreach ($pdo->query($sql) as $row) {
+						   		echo '<tr>';
+							   	echo '<td>'. $row['name'] . '</td>';
+							   	echo '<td>'. $row['email'] . '</td>';
+							   	echo '<td>'. $row['mobile'] . '</td>';
+							   	echo '<td width=250>';
+							   	echo '<a class="btn" href="read.php?id='.$row['id'].'">Read</a>';
+							   	echo '&nbsp;';
+							   	echo '<a class="btn btn-success" href="update.php?id='.$row['id'].'">Update</a>';
+							   	echo '&nbsp;';
+							   	echo '<a class="btn btn-danger" href="delete.php?id='.$row['id'].'">Delete</a>';
+							   	echo '</td>';
+							   	echo '</tr>';
+					   }
+					   Database::disconnect();
+					  ?>
+				      </tbody>
+	            </table>
+    	</div>
+    </div> <!-- /container -->
+  </body>
+</html>
